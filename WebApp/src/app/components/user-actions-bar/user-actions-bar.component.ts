87e6197791659423ba69db7914fabc398a849fae
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2024-09-18 10:14:30 -0500

    Finalized Create a User Frontend Functionality

--- WebApp/src/app/components/user-actions-bar/user-actions-bar.component.ts
+++ WebApp/src/app/components/user-actions-bar/user-actions-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'user-actions-bar',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
   templateUrl: './user-actions-bar.component.html',
   styleUrl: './user-actions-bar.component.css',
 })
-export class UserActionsBar {}
+export class UserActionsBar {
+  constructor(private router: Router) {}
+
+  openUserForm() {
+    this.router.navigate(['/users/new']);
+  }
+}
